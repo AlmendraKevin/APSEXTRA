ff54a34ed6d4cb7b9818cf93e32d1680a694490d
AlmendraKevin <kevin.almendra@uabc.edu.mx>
2021-06-15 13:07:05 -0700

    actualizar vista

--- src/main/java/com/uabc/edu/devsurvivor/security/WebSecurityConfig.java
+++ src/main/java/com/uabc/edu/devsurvivor/security/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                .antMatchers("/css/**","/js/**").permitAll()
+                .antMatchers("/","/css/**","/js/**").permitAll()
                 .antMatchers("/*.png", "/*.jpg", "/*.jpeg").permitAll()
                 .anyRequest()
                 .authenticated()
