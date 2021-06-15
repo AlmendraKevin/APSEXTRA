5a5b5b0fea8db40079f4ba5900205e9db4d846b7
AlmendraKevin <kevin.almendra@uabc.edu.mx>
2021-06-15 11:18:23 -0700

    Actualizacion de la base de datos
    Se crearon y actualizaron los schemas de la base de datos y creacion del servico

--- src/main/java/com/uabc/edu/devsurvivor/service/ZapateriaService.java
+++ src/main/java/com/uabc/edu/devsurvivor/service/ZapateriaService.java
@@ -14,16 +14,48 @@ public class ZapateriaService {
     @Autowired
     ZapatoRepository repository;
 
-    public List<Zapato> getShoes(){return (List<Zapato>) repository.findAll();}
+    public List<Zapato> getShoes() {
+        return (List<Zapato>) repository.findAll();
+    }
 
-    public Zapato getShoesById(Long id){
+    public Zapato getShoesById(Long id) {
         Optional<Zapato> shoe = repository.findById(id);
-        if (shoe.isPresent()){
+        if (shoe.isPresent()) {
             return repository.findById(id).get();
         }
         return shoe.get();
     }
-    
 
+    public Zapato saveShoe(Zapato z) {
+        if (z.getIdZapato() == 0L) {
+            z = repository.save(z);
+            return z;
+        } else {
+            Optional<Zapato> shoe = repository.findById(z.getIdZapato());
+            if (shoe.isPresent()) {
+                Zapato newShoe = shoe.get();
+                newShoe.setIdZapato(z.getIdZapato());
+                newShoe.setModeloZapato(z.getModeloZapato());
+                newShoe.setColorZapato(z.getColorZapato());
+                newShoe.setTallaZapato(z.getTallaZapato());
+                newShoe.setExistenciaZapato(z.getExistenciaZapato());
+                newShoe.setCostoZapato(z.getCostoZapato());
+                newShoe.setUbicacionZapato(z.getUbicacionZapato());
+                newShoe.setImg(z.getImg());
+                newShoe = repository.save(newShoe);
+                return newShoe;
+            } else {
+                z = repository.save(z);
+                return z;
+            }
+        }
+    }
+
+    public void deleteShoe(Long id) {
+        Optional<Zapato> zapato = repository.findById(id);
+        if (zapato.isPresent()) {
+            repository.deleteById(id);
+        }
+    }
 
 }
--- src/main/java/com/uabc/edu/devsurvivor/web/MainController.java
+++ src/main/java/com/uabc/edu/devsurvivor/web/MainController.java
@@ -1,4 +1,18 @@
 package com.uabc.edu.devsurvivor.web;
 
+import com.uabc.edu.devsurvivor.service.ZapateriaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class MainController {
+
+    private ZapateriaService service;
+
+    @RequestMapping
+    public String home(){return "home";}
+
+    @RequestMapping("/login")
+    public String login(){return "login";}
 }
